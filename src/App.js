f52e78a45afe94bceee19c143a6da18229d76e1b
Nannuri Pranay Kumar Reddy <npranay.kreddy.eee21@itbhu.ac.in>
2024-06-24 16:37:15 +0530

    layout is added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from 'react';
 import './App.css';
 //import {Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Entry } from './page/entry/Entry.page.js';
+import { DefaultLayout } from './layout/DefaultLayout.js';
 
 function App() {
   return (
     <div className="App">
-      <Entry/>
+      {/* <Entry/> */}
+      <DefaultLayout>
+        //Dashboard
+      </DefaultLayout>
     </div>
   );
 }
